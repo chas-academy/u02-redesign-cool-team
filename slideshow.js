4f6dbe02a0c2c6f6c1bfb8d89dbbf838463a8b61
frankemon <70698241+frankemon@users.noreply.github.com>
2020-10-25 20:29:03 +0100

    slide show arrows work

--- slideshow.js
+++ slideshow.js
@@ -1,9 +1,36 @@
 const leftArrow = document.querySelector('.slide-arrow-left');
-const lastSlide = document.querySelector('.slide-active');
-const currentSlide = document.querySelector('.slide-3');
+const rightArrow = document.querySelector('.slide-arrow-right');
+const slide1 = document.querySelector('.slide-1');
+const slide2 = document.querySelector('.slide-2');
+const slide3 = document.querySelector('.slide-3');
 
+const slides = [ 
+  slide1,
+  slide2,
+  slide3
+];
+let slideCounter = 0; 
 
 leftArrow.onclick = function (e) {
+    slideCounter -= 1;
+    if(slideCounter === -1) {
+        slideCounter = slides.length -1;
+    }
+
+    const lastSlide = document.querySelector('.slide-active');
+    const currentSlide = slides[slideCounter];
+    lastSlide.classList.remove("slide-active");
+    currentSlide.classList.add("slide-active");
+}
+
+rightArrow.onclick = function (e) {
+    slideCounter += 1;
+    if(slideCounter === slides.length) {
+        slideCounter = 0;
+    }
+
+    const lastSlide = document.querySelector('.slide-active');
+    const currentSlide = slides[slideCounter];
     lastSlide.classList.remove("slide-active");
     currentSlide.classList.add("slide-active");
 }
